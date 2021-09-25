59fba933a24ca009319dd59ecb3ea458a6fba048
Sergey Lushkin <serg-nn@inbox.ru>
2021-09-25 11:16:04 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let a = 'hello world';
+// console.log(a);
+
+// const earth = "Земля";
+// let actualTimeUser;
+// let countPublic;
+// let isCash;
+// let name;
+// let secondName;
+// let surname;
+
+// console.log(earth, actualTimeUser, countPublic, isCash, name, secondName, surname); 
+const myName="Sergey";
+
+
